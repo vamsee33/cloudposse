dc9be89dd29de1f222307cc5b93919d978b95c29
Vamsee Kumar Rayudu <123220568+vamsee33@users.noreply.github.com>
2025-07-12 15:06:10 +0530

    Add files via upload

--- components/terraform/redis-elasticache/main.tf
+++ components/terraform/redis-elasticache/main.tf
@@ -2,15 +2,53 @@ provider "aws" {
   region = var.region
 }
 
-# Secure, serverless Redis cluster using outputs from a separate VPC component
+# Create VPC
+module "vpc" {
+  source  = "cloudposse/vpc/aws"
+  version = "2.2.0"
+
+  ipv4_primary_cidr_block = var.vpc_cidr_block
+  context = module.this.context
+}
+
+# Create private subnets
+module "subnets" {
+  source  = "cloudposse/dynamic-subnets/aws"
+  version = "2.4.2"
+
+  availability_zones   = var.availability_zones
+  vpc_id               = module.vpc.vpc_id
+  igw_id               = [module.vpc.igw_id]
+  ipv4_cidr_block      = [module.vpc.vpc_cidr_block]
+  nat_gateway_enabled  = false
+  nat_instance_enabled = false
+  context = module.this.context
+}
+
+# Private Route53 zone for Redis
+resource "aws_route53_zone" "private" {
+  name = format("elasticache-redis-terratest-%s.testing.cloudposse.co", try(module.this.attributes[0], "default"))
+  vpc {
+    vpc_id = module.vpc.vpc_id
+  }
+}
+
+# CloudWatch logs for Redis
+module "cloudwatch_logs" {
+  source  = "cloudposse/cloudwatch-logs/aws"
+  version = "0.6.8"
+  context = module.this.context
+}
+
+# Secure, serverless Redis cluster
 module "redis" {
   source = "cloudposse/redis-serverless/aws"
 
   serverless_enabled                  = var.serverless_enabled
-  zone_id                             = var.zone_id
-  vpc_id                              = var.vpc_id
-  allowed_security_groups             = var.allowed_security_groups
-  subnets                             = var.subnet_ids
+  zone_id                             = [aws_route53_zone.private.id]
+  vpc_id                              = module.vpc.vpc_id
+  allowed_security_groups             = [module.vpc.vpc_default_security_group_id]
+  subnets                             = module.subnets.private_subnet_ids
   serverless_major_engine_version     = var.serverless_major_engine_version
   at_rest_encryption_enabled          = var.at_rest_encryption_enabled
   serverless_cache_usage_limits       = var.serverless_cache_usage_limits
